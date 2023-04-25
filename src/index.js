3ae168231bfd716d1a33330bb9473df825300489
lauragift21 <laurandidi21@gmail.com>
2023-04-25 03:15:04 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
