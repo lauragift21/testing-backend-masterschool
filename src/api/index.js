f1a2904e69ddc4d11c78e2f75d5961ff1200c33b
lauragift21 <laurandidi21@gmail.com>
2023-04-25 04:08:07 +0200

    add user controller

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const emojis = require('./emojis');
+const user = require('./user');
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get('/', (req, res) => {
 });
 
 router.use('/emojis', emojis);
+router.use('/user', user);
 
 module.exports = router;
---
+++ src/api/user.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const user = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+
+mongoose
+  .connect(`mongodb+srv://${user}:${password}@cluster0.yss5oph.mongodb.net/?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+// Define schema and model for the database
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const router = express.Router();
+
+// use body-parser middleware
+router.use(bodyParser.json());
+
+// Create a new user
+router.post('/', async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = new User({ name, email, password });
+  try {
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Get a list of users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get a specific user by ID
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Update a specific user by ID
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(id, { name, email, password }, { new: true });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Delete a specific user by ID
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
